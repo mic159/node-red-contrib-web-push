aca7696c485fb7fd59a776180a4957f30efa5ba4
Michael <mic159@gmail.com>
2018-06-20 18:20:53 +1000

    Use credentials to store private key

--- vapid-configuration.js
+++ vapid-configuration.js
@@ -1,9 +1,17 @@
 module.exports = function(RED) {
-    function VapidConfiguration(n) {
-        RED.nodes.createNode(this,n);
-        this.subject = n.subject;
-        this.publicKey = n.publicKey;
-        this.privateKey = n.privateKey;
+  function VapidConfiguration(n) {
+    RED.nodes.createNode(this, n);
+    this.subject = n.subject;
+    this.publicKey = n.publicKey;
+    this.privateKey = this.credentials.privateKey;
+  }
+  RED.nodes.registerType(
+    'vapid-configuration',
+    VapidConfiguration,
+    {
+      credentials: {
+        privateKey: {type: 'text'},
+      }
     }
-    RED.nodes.registerType("vapid-configuration", VapidConfiguration);
+  );
 }
